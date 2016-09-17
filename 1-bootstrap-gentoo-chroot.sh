2fee835c0f4551857918a6416c4ee7e7f44c7b4f
Jeff 'Raid' Baitis <jeff@baitis.net>
2016-09-17 12:36:05 -0700

    include kernel configuration for virtualbox

--- 1-bootstrap-gentoo-chroot.sh
+++ 1-bootstrap-gentoo-chroot.sh
@@ -1,3 +1,6 @@
+
+# Need 3Gig ram
+
 #!/bin/bash -e
 
 set -x # Trace execution
@@ -5,36 +8,22 @@ set -x # Trace execution
 . /etc/profile
 env-update
 
-# Beware of OOM killer
-dd if=/dev/zero of=/swap bs=1M count=4096
-mkswap /swap
-swapon /swap
-echo "/swap none swap defaults" > /etc/fstab
-
 emerge --sync
 
 gunzip -c /proc/config.gz > /usr/src/livecd.config
-emerge hardened-sources genkernel grub
+emerge genkernel grub
+cp config-4.4.6-vbox /usr/src/linux/.config
 cd /usr/src/linux
 
-# This config works for Hetzner CX10. YMMV. Change accordingly.
-make defconfig kvmconfig
-echo "
-CONFIG_SCSI_LOWLEVEL=y
-CONFIG_SCSI_VIRTIO=y
-CONFIG_VIRTIO_INPUT=y
-CONFIG_VIRTIO_MMIO=y
-CONFIG_VIRTIO_MMIO_CMDLINE_DEVICES=y
-" >> .config
-make olddefconfig
+make oldconfig
 cp .config /usr/src/kernel.config
 genkernel --kernel-config=/usr/src/kernel.config all
 
-grub2-install /dev/sda
+grub-install /dev/sda
 
 # Care about networking setup. Disable upredictable "predictable ifnames"
 echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX rootfstype=ext4 net.ifnames=0"' >> /etc/default/grub
-grub2-mkconfig -o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # Configure networking
 ln -s net.lo /etc/init.d/net.eth0
@@ -43,23 +32,38 @@ echo 'config_eth0="dhcp"' >> /etc/conf.d/net
 
 rc-update add sshd default
 
+eselect profile set 6
+
+exit 0 
+
+echo <<EOF > /etc/portage/make.conf
+CFLAGS="-O2 -pipe -march=native"
+CXXFLAGS="${CFLAGS}"
+CHOST="x86_64-pc-linux-gnu"
+USE="bindist"
+PORTDIR="/usr/portage"
+DISTDIR="${PORTDIR}/distfiles"
+PKGDIR="${PORTDIR}/packages"
+MAKEOPTS="-j5"
+EOF
+
 # Download my SSH keys. If you are not me, you want to change this
-mkdir -p /root/.ssh
-wget https://gist.github.com/andrey-utkin/2bb57efd85387edad34e/raw/b9d67d781f70699474154522eb84ff8bd528864d/authorized_keys -O /root/.ssh/authorized_keys
+#mkdir -p /root/.ssh
+#wget https://gist.github.com/andrey-utkin/2bb57efd85387edad34e/raw/b9d67d781f70699474154522eb84ff8bd528864d/authorized_keys -O /root/.ssh/authorized_keys
+
+# eselect profile list
+# [6] kde
 
 # Install handy stuff
 emerge -v \
   gentoolkit \
-  \
+  sudo \
   syslog-ng \
   logrotate \
   \
   vim \
-  app-misc/screen \
+  #app-misc/screen \
   tmux \
-  bash-completion \
-
-
-wget https://github.com/decent-im/gebootstrap/raw/master/2-install-decent-im.sh
-chmod a+x 2-install-decent-im.sh
-./2-install-decent-im.sh
+  app-emulation/virtualbox-guest-additions
+  www-client/chromium
+# ratpoison
